31d49247aeed727e4e7b2fb76eef8045b90acb5a
iotarex <z_series@tuta.io>
2020-01-15 03:30:39 -0800

    Added the decryption logic and functionality

--- NDEFCom/Pages/NdefEdit.cs
+++ NDEFCom/Pages/NdefEdit.cs
@@ -27,6 +27,55 @@ namespace NDEFCom.Pages
 
             uxNdefEditor = FindViewById<EditText>(Resource.Id.uxNdefEditor);
             uxNdefEditor.Text = NdefPayload;
+
+            CheckEncrypted();
+        }
+
+        private void CheckEncrypted()
+        {
+            if(NdefPayload.Length < 27)
+            {
+                return;
+            }
+            else if(!NdefPayload.Substring(0, 27).Equals("-----BEGIN PGP MESSAGE-----"))
+            {
+                return;
+            }
+            else
+            {
+                Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                AlertDialog alert = dialog.Create();
+                alert.SetTitle("Encrypted payload detected");
+                alert.SetMessage("Would you like to attempt decryption with OpenKeychain?");
+                alert.SetButton("Yes", (c, ev) =>
+                {
+                    ClipboardManager clipboard = (ClipboardManager)GetSystemService(Context.ClipboardService);
+                    ClipData clip = ClipData.NewPlainText("payload", NdefPayload);
+                    clipboard.PrimaryClip = clip;
+
+                    Intent intent = Android.App.Application.Context.PackageManager.GetLaunchIntentForPackage("org.sufficientlysecure.keychain.debug");
+
+                    if(intent != null)
+                    {
+                        intent.AddFlags(ActivityFlags.NewTask);
+                        StartActivity(intent);
+                    }
+                    else
+                    {
+                        intent = new Intent(Intent.ActionView);
+                        intent.AddFlags(ActivityFlags.NewTask);
+                        intent.SetData(Android.Net.Uri.Parse("https://f-droid.org/en/packages/org.sufficientlysecure.keychain/"));
+                        StartActivity(intent);
+                    }
+                });
+
+                alert.SetButton2("No", (c, ev) =>
+                {
+                    return;
+                });
+
+                alert.Show();
+            }
         }
     }
 }
